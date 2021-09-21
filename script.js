caad7f79f851bd7e8a15a8729d4f4aa2de6d254d
bhanu-kiran0183 <bhanu.thathari96@gmail.com>
2021-09-21 19:06:11 +0530

    added form on click of bookmark

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnAddBookmark = document.getElementById("show--button");
 const websiteName = document.getElementById("website-name");
 const websiteUrl = document.getElementById("website-url");
 const modalContainer = document.getElementById("modal");
+const bookmarkForm = document.getElementById("form-data");
 labelCloseForm.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("write code for ___");
@@ -11,13 +12,24 @@ labelCloseForm.addEventListener("click", function (e) {
 });
 btnAddBookmark.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("write code for ___");
+  //console.log("write code for ___");
   document.getElementById("modal").classList.add("show-modal");
   websiteName.focus();
 });
 //closing the form if the user clicks anywhere on the screen
 window.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(e.target);
+  //console.log(e.target);
   e.target === modal ? modalContainer.classList.remove("show-modal") : false;
 });
+
+bookmarkForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //console.log(e);
+  const nameValue = websiteName.value;
+  let urlValue = websiteUrl.value;
+  console.log(nameValue, urlValue);
+  console.log("hjgjhgjh");
+});
+
+console.log(websiteUrl.value, websiteName);
